9ffce948e4557f217fad157f4ea9cbee25741629
mcoracin <mcoracin@semtech.com>
2018-07-16 11:07:01 +0200

    SPI: removed sx1262fe test from spi test app

--- libloragw/tst/test_loragw_spi.c
+++ libloragw/tst/test_loragw_spi.c
@@ -43,7 +43,6 @@ int main()
 {
     void *spi_target = NULL;
     uint8_t data = 0;
-    uint8_t buff[16];
     uint8_t test_buff[BUFF_SIZE];
     uint8_t read_buff[BUFF_SIZE];
     int cycle_number = 0;
@@ -64,9 +63,7 @@ int main()
     lgw_spi_r(spi_target, LGW_SPI_MUX_TARGET_SX1302, SX1302_REG_COMMON + 5, &data);
     printf("SX1302 version: 0x%02X\n", data);
 
-#if 1
     lgw_spi_r(spi_target, LGW_SPI_MUX_TARGET_SX1302, SX1302_REG_AGC_MCU + 0, &data);
-    printf("agc_mcu.ctrl=0x%02X\n", data);
     lgw_spi_w(spi_target, LGW_SPI_MUX_TARGET_SX1302, SX1302_REG_AGC_MCU + 0, 0x06); /* mcu_clear, host_prog */
 
     /* databuffer R/W stress test */
@@ -98,45 +95,6 @@ int main()
             ++cycle_number;
         }
     }
-
-#else
-    lgw_spi_w(spi_target, LGW_SPI_MUX_TARGET_SX1302, 0x5783, 0x0C); /* Set radio_en & radio_rst to 1 */
-    lgw_spi_r(spi_target, LGW_SPI_MUX_TARGET_SX1302, 0x5783, &data);
-    printf("SX1302 agc_mcu.rf_en_a: 0x%02X\n", data);
-
-    buff[0] = 0x00;
-    sx1262fe_write_command(spi_target, 0x80, buff, 1); /* SetStandby STDBY_RC */
-
-    buff[0] = 0x00;
-    sx1262fe_read_command(spi_target, 0xC0, buff, 1); /* GetStatus */
-    printf("SX1262FE status: 0x%02X\n", buff[0]);
-
-    buff[0] = 0x00;
-    sx1262fe_write_command(spi_target, 0x8A, buff, 1); /* SetPacketType FSK */
-
-    buff[0] = 0x36;
-    buff[1] = 0x48;
-    buff[2] = 0x00;
-    buff[3] = 0x00;
-    sx1262fe_write_command(spi_target, 0x86, buff, 4); /* SetRfFrequency */
-
-    buff[0] = 0x0E;
-    buff[1] = 0x02;
-    sx1262fe_write_command(spi_target, 0x8E, buff, 2); /* SetTxParams (power, RAMP_40U) */
-
-    buff[0] = 0x04;
-    buff[1] = 0x07;
-    buff[2] = 0x00;
-    buff[3] = 0x01;
-    sx1262fe_write_command(spi_target, 0x95, buff, 4); /* SetPaOpt */
-
-    buff[0] = 0x00;
-    sx1262fe_write_command(spi_target, 0xD1, buff, 1); /* SetTxContinuousWave */
-
-    buff[0] = 0x00;
-    sx1262fe_read_command(spi_target, 0xC0, buff, 1); /* GetStatus */
-    printf("SX1262FE status: 0x%02X\n", buff[0]);
-#endif
 
     lgw_spi_close(spi_target);
     printf("End of test for loragw_spi.c\n");
