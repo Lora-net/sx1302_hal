40afed4e4772c6e4bae7fde5b96d66d727d30a23
mcoracin <mcoracin@semtech.com>
2018-08-21 17:17:11 +0200

    SX1302: added sx1302 driver file

---
+++ libloragw/inc/loragw_sx1302.h
@@ -0,0 +1,53 @@
+/*
+ / _____)             _              | |
+( (____  _____ ____ _| |_ _____  ____| |__
+ \____ \| ___ |    (_   _) ___ |/ ___)  _ \
+ _____) ) ____| | | || |_| ____( (___| | | |
+(______/|_____)_|_|_| \__)_____)\____)_| |_|
+  (C)2018 Semtech
+
+Description:
+    TODO
+
+License: Revised BSD License, see LICENSE.TXT file include in the project
+*/
+
+
+#ifndef _LORAGW_SX1302_H
+#define _LORAGW_SX1302_H
+
+/* -------------------------------------------------------------------------- */
+/* --- DEPENDANCIES --------------------------------------------------------- */
+
+#include <stdint.h>     /* C99 types*/
+
+#include "config.h"     /* library configuration options (dynamically generated) */
+
+/* -------------------------------------------------------------------------- */
+/* --- PUBLIC CONSTANTS ----------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/* --- PUBLIC MACROS -------------------------------------------------------- */
+
+#define REG_SELECT(rf_chain, a, b) ((rf_chain == 0) ? a : b)
+
+/* -------------------------------------------------------------------------- */
+/* --- PUBLIC TYPES --------------------------------------------------------- */
+
+typedef enum {
+    SX1302_RADIO_TYPE_SX1250,   /* sx1250 */
+    SX1302_RADIO_TYPE_SX125X    /* sx1255/1257 */
+} sx1302_radio_type_t;
+
+/* -------------------------------------------------------------------------- */
+/* --- PUBLIC FUNCTIONS PROTOTYPES ------------------------------------------ */
+
+int sx1302_radio_clock_select(uint8_t rf_chain);
+int sx1302_radio_reset(uint8_t rf_chain, sx1302_radio_type_t type);
+int sx1302_radio_set_mode(uint8_t rf_chain, sx1302_radio_type_t type);
+
+int sx1302_clock_enable();
+
+#endif
+
+/* --- EOF ------------------------------------------------------------------ */
---
+++ libloragw/src/loragw_sx1302.c
@@ -0,0 +1,136 @@
+/*
+ / _____)             _              | |
+( (____  _____ ____ _| |_ _____  ____| |__
+ \____ \| ___ |    (_   _) ___ |/ ___)  _ \
+ _____) ) ____| | | || |_| ____( (___| | | |
+(______/|_____)_|_|_| \__)_____)\____)_| |_|
+  (C)2018 Semtech
+
+Description:
+    TODO
+
+License: Revised BSD License, see LICENSE.TXT file include in the project
+*/
+
+
+/* -------------------------------------------------------------------------- */
+/* --- DEPENDANCIES --------------------------------------------------------- */
+
+#include <stdint.h>     /* C99 types */
+#include <stdio.h>      /* printf fprintf */
+#include <stdlib.h>     /* malloc free */
+#include <unistd.h>     /* lseek, close */
+#include <fcntl.h>      /* open */
+#include <string.h>     /* memset */
+
+#include <sys/ioctl.h>
+#include <linux/spi/spidev.h>
+
+#include "loragw_reg.h"
+#include "loragw_aux.h"
+#include "loragw_hal.h"
+#include "loragw_sx1302.h"
+
+/* -------------------------------------------------------------------------- */
+/* --- PRIVATE MACROS ------------------------------------------------------- */
+
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
+#if DEBUG_REG == 1
+    #define DEBUG_MSG(str)                fprintf(stderr, str)
+    #define DEBUG_PRINTF(fmt, args...)    fprintf(stderr,"%s:%d: "fmt, __FUNCTION__, __LINE__, args)
+    #define CHECK_NULL(a)                if(a==NULL){fprintf(stderr,"%s:%d: ERROR: NULL POINTER AS ARGUMENT\n", __FUNCTION__, __LINE__);return LGW_SPI_ERROR;}
+#else
+    #define DEBUG_MSG(str)
+    #define DEBUG_PRINTF(fmt, args...)
+    #define CHECK_NULL(a)                if(a==NULL){return LGW_SPI_ERROR;}
+#endif
+
+/* -------------------------------------------------------------------------- */
+/* --- PRIVATE CONSTANTS ---------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/* --- INTERNAL SHARED VARIABLES -------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/* --- PUBLIC FUNCTIONS DEFINITION ------------------------------------------ */
+
+int sx1302_radio_clock_select(uint8_t rf_chain) {
+    /* Check input parameters */
+    if (rf_chain >= LGW_RF_CHAIN_NB)
+    {
+        DEBUG_MSG("ERROR: invalid RF chain\n");
+        return LGW_REG_ERROR;
+    }
+
+    return LGW_REG_SUCCESS;
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+int sx1302_radio_reset(uint8_t rf_chain, sx1302_radio_type_t type) {
+    uint16_t reg_radio_en;
+    uint16_t reg_radio_rst;
+
+    /* Check input parameters */
+    if (rf_chain >= LGW_RF_CHAIN_NB)
+    {
+        DEBUG_MSG("ERROR: invalid RF chain\n");
+        return LGW_REG_ERROR;
+    }
+    if ((type != SX1302_RADIO_TYPE_SX1250) && (type != SX1302_RADIO_TYPE_SX125X))
+    {
+        DEBUG_MSG("ERROR: invalid radio mode\n");
+        return LGW_REG_ERROR;
+    }
+
+    /* Enable the radio */
+    reg_radio_en = REG_SELECT(rf_chain, SX1302_REG_AGC_MCU_RF_EN_A_RADIO_EN, SX1302_REG_AGC_MCU_RF_EN_B_RADIO_EN);
+    lgw_reg_w(reg_radio_en, 0x01);
+
+    /* Select the proper reset sequence depending on the radio type */
+    reg_radio_rst = REG_SELECT(rf_chain, SX1302_REG_AGC_MCU_RF_EN_A_RADIO_RST, SX1302_REG_AGC_MCU_RF_EN_B_RADIO_RST);
+    lgw_reg_w(reg_radio_rst, 0x01);
+    wait_ms(500);
+    lgw_reg_w(reg_radio_rst, 0x00);
+    wait_ms(10);
+    switch (type) {
+        case SX1302_RADIO_TYPE_SX125X:
+            /* Do nothing */
+            DEBUG_PRINTF("INFO: reset sx125x (RADIO_%s) done\n", REG_SELECT(rf_chain, "A", "B"));
+            break;
+        case SX1302_RADIO_TYPE_SX1250:
+            lgw_reg_w(reg_radio_rst, 0x01);
+            DEBUG_PRINTF("INFO: reset sx1250 (RADIO_%s) done\n", REG_SELECT(rf_chain, "A", "B"));
+            break;
+        default:
+            return LGW_REG_ERROR;
+    }
+
+    return LGW_REG_SUCCESS;
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+int sx1302_radio_set_mode(uint8_t rf_chain, sx1302_radio_type_t type) {
+    /* Check input parameters */
+    if (rf_chain >= LGW_RF_CHAIN_NB)
+    {
+        DEBUG_MSG("ERROR: invalid RF chain\n");
+        return LGW_REG_ERROR;
+    }
+    if ((type != SX1302_RADIO_TYPE_SX1250) && (type != SX1302_RADIO_TYPE_SX125X))
+    {
+        DEBUG_MSG("ERROR: invalid radio mode\n");
+        return LGW_REG_ERROR;
+    }
+
+    return LGW_REG_SUCCESS;
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+int sx1302_clock_enable() {
+    return LGW_REG_SUCCESS;
+}
+
+/* --- EOF ------------------------------------------------------------------ */
