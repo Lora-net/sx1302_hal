e0e1bd81ba704ad6d574de4a2ce3794a41ba4976
Michael Coracin <mcoracin@semtech.com>
2019-07-11 14:57:10 +0200

    Added DEBUG_SX1302 flag for sx1302 module debug logs

--- libloragw/src/loragw_sx1302_timestamp.c
+++ libloragw/src/loragw_sx1302_timestamp.c
@@ -33,9 +33,9 @@ License: Revised BSD License, see LICENSE.TXT file include in the project
 /* --- PRIVATE MACROS ------------------------------------------------------- */
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
-#if DEBUG_REG == 1
+#if DEBUG_SX1302 == 1
     #define DEBUG_MSG(str)                fprintf(stderr, str)
-    #define DEBUG_PRINTF(fmt, args...)    fprintf(stderr,"%s:%d: "fmt, __FUNCTION__, __LINE__, args)
+    #define DEBUG_PRINTF(fmt, args...)    fprintf(stderr, fmt, args)
     #define CHECK_NULL(a)                if(a==NULL){fprintf(stderr,"%s:%d: ERROR: NULL POINTER AS ARGUMENT\n", __FUNCTION__, __LINE__);return LGW_REG_ERROR;}
 #else
     #define DEBUG_MSG(str)
