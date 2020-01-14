085c2437c2b35678e587e5406b4695aa85141ef8
Michael Coracin <mcoracin@semtech.com>
2020-01-14 15:11:39 +0100

    HAL: fix endless loop in sx1302_tx_abort() if access to SPI device fails

--- libloragw/src/loragw_sx1302.c
+++ libloragw/src/loragw_sx1302.c
@@ -1945,7 +1945,7 @@ uint8_t sx1302_tx_status(uint8_t rf_chain) {
 
     err = lgw_reg_r(SX1302_REG_TX_TOP_TX_FSM_STATUS_TX_STATUS(rf_chain), &read_value);
     if (err != LGW_REG_SUCCESS) {
-        printf("ERROR: Failed to read TX STATUS");
+        printf("ERROR: Failed to read TX STATUS\n");
         return TX_STATUS_UNKNOWN;
     }
 
@@ -1972,13 +1972,20 @@ uint8_t sx1302_rx_status(uint8_t rf_chain) {
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 int sx1302_tx_abort(uint8_t rf_chain) {
-    lgw_reg_w(SX1302_REG_TX_TOP_TX_TRIG_TX_TRIG_IMMEDIATE(rf_chain), 0x00);
-    lgw_reg_w(SX1302_REG_TX_TOP_TX_TRIG_TX_TRIG_DELAYED(rf_chain), 0x00);
-    lgw_reg_w(SX1302_REG_TX_TOP_TX_TRIG_TX_TRIG_GPS(rf_chain), 0x00);
+    int err;
+    uint8_t tx_status;
+
+    err  = lgw_reg_w(SX1302_REG_TX_TOP_TX_TRIG_TX_TRIG_IMMEDIATE(rf_chain), 0x00);
+    err |= lgw_reg_w(SX1302_REG_TX_TOP_TX_TRIG_TX_TRIG_DELAYED(rf_chain), 0x00);
+    err |= lgw_reg_w(SX1302_REG_TX_TOP_TX_TRIG_TX_TRIG_GPS(rf_chain), 0x00);
+    if (err != LGW_REG_SUCCESS) {
+        printf("ERROR: Failed to stop TX trigger\n");
+        return err;
+    }
 
     do {
         wait_ms(1);
-    } while (sx1302_tx_status(rf_chain) != TX_FREE);
+    } while ((tx_status = sx1302_tx_status(rf_chain)) != TX_FREE && tx_status != TX_STATUS_UNKNOWN);
 
     return LGW_REG_SUCCESS;
 }
