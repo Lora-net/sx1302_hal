d600df07e54f6b2e7fd500f197e41418a83d80e4
Michael Coracin <mcoracin@semtech.com>
2020-05-26 17:16:13 +0200

    Updated ARB firmware to version 2

--- libloragw/src/loragw_hal.c
+++ libloragw/src/loragw_hal.c
@@ -83,7 +83,7 @@ License: Revised BSD License, see LICENSE.TXT file include in the project
 /* --- PRIVATE CONSTANTS & TYPES -------------------------------------------- */
 
 #define FW_VERSION_AGC      1 /* Expected version of AGC firmware */
-#define FW_VERSION_ARB      1 /* Expected version of arbiter firmware */
+#define FW_VERSION_ARB      2 /* Expected version of arbiter firmware */
 
 /* Useful bandwidth of SX125x radios to consider depending on channel bandwidth */
 /* Note: the below values come from lab measurements. For any question, please contact Semtech support */
