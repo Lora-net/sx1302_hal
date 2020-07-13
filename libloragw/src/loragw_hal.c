ed92aa6ffb9584ca7d621c79969ac1509a977e2e
Michael Coracin <mcoracin@semtech.com>
2020-07-13 18:26:48 +0200

    HAL: check that sx1261 is enabled to start spectral scan

--- libloragw/src/loragw_hal.c
+++ libloragw/src/loragw_hal.c
@@ -1542,7 +1542,10 @@ int lgw_set_xtal_correct(bool is_valid, double xtal_correction) {
 int lgw_spectral_scan(uint32_t freq_hz, uint16_t nb_scan, int16_t levels_dbm[static LGW_SPECTRAL_SCAN_RESULT_SIZE], uint16_t results[static LGW_SPECTRAL_SCAN_RESULT_SIZE]) {
     int err;
 
-    //TODO: check that SX1261 radio is initialized (LBT_ENABLE to be renamed)
+    if (CONTEXT_SX1261.enable != true) {
+        printf("ERROR: sx1261 is not enabled, no spectral scan\n");
+        return LGW_HAL_ERROR;
+    }
 
     err = sx1261_set_rx_params(freq_hz, BW_125KHZ);
     if (err != LGW_REG_SUCCESS) {
