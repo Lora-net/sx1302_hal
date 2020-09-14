a5ec10a8227994cc5e1ebd550a863d90a807d9b8
Michael Coracin <mcoracin@semtech.com>
2020-09-14 10:55:04 +0200

    TST: disabled USB support for test_loragw_capture_ram (EDEL-53)

--- libloragw/tst/test_loragw_capture_ram.c
+++ libloragw/tst/test_loragw_capture_ram.c
@@ -23,8 +23,6 @@ License: Revised BSD License, see LICENSE.TXT file include in the project
     #define _XOPEN_SOURCE 500
 #endif
 
-
-// #include <stdint.h>
 #include <stdio.h>      /* printf */
 #include <stdlib.h>
 #include <signal.h>     /* sigaction */
@@ -41,12 +39,11 @@ License: Revised BSD License, see LICENSE.TXT file include in the project
 /* -------------------------------------------------------------------------- */
 /* --- PRIVATE MACROS ------------------------------------------------------- */
 
-#define DEBUG_MSG(str)                fprintf(stderr, str)
+#define DEBUG_MSG(str) fprintf(stderr, str)
 
 #define COM_TYPE_DEFAULT LGW_COM_SPI
 #define COM_PATH_DEFAULT "/dev/spidev0.0"
 
-#define FULL_INIT 0
 #define CAPTURE_RAM_SIZE 0x4000
 
 /* -------------------------------------------------------------------------- */
@@ -58,21 +55,6 @@ static int quit_sig = 0; /* 1 -> application terminates without shutting down th
 
 uint32_t sampling_frequency[] = {4e6, 4e6, 4e6, 4e6, 4e6, 4e6, 4e6, 0, 0, 1e6, 125e3, 125e3, 125e3, 125e3, 125e3, 125e3, 125e3, 125e3, 8e6, 125e3, 125e3, 125e3, 0, 32e6, 32e6, 0, 32e6, 32e6, 0, 32e6, 32e6, 32e6};
 
-#if FULL_INIT
-#include "src/text_agc_sx1250_27_Nov_1.var"
-#include "src/text_agc_sx1257_19_Nov_1.var"
-#include "src/text_arb_sx1302_13_Nov_3.var"
-
-#define FW_VERSION_CAL      0 /* Expected version of calibration firmware */
-#define FW_VERSION_AGC      1 /* Expected version of AGC firmware */
-#define FW_VERSION_ARB      1 /* Expected version of arbiter firmware */
-
-static bool rf_enable[LGW_RF_CHAIN_NB];
-static uint32_t rf_rx_freq[LGW_RF_CHAIN_NB]; /* absolute, in Hz */
-static lgw_radio_type_t rf_radio_type[LGW_RF_CHAIN_NB];
-static uint8_t rf_clkout = 0;
-#endif
-
 /* -------------------------------------------------------------------------- */
 /* --- PRIVATE FUNCTIONS ---------------------------------------------------- */
 
@@ -81,8 +63,7 @@ void usage(void)
 {
     printf("Available options:\n");
     printf(" -h print this help\n");
-    printf(" -u        Set COM type as USB (default is SPI)\n");
-    printf(" -d [path] Path to the COM interface\n");
+    printf(" -d [path] Path to the SPI interface (USB is not supported)\n");
     printf("            => default path: " COM_PATH_DEFAULT "\n");
     printf(" -s <uint> Capture source [0..31]\n");
 }
@@ -108,9 +89,6 @@ int main(int argc, char **argv)
     uint8_t capture_source = 0;
     uint16_t period_value = 0;
     int16_t real = 0, imag = 0;
-#if FULL_INIT
-    uint32_t val1, val2;
-#endif
     uint8_t capture_ram_buffer[CAPTURE_RAM_SIZE];
 
     static struct sigaction sigact; /* SIGQUIT&SIGINT&SIGTERM signal handling */
@@ -127,15 +105,11 @@ int main(int argc, char **argv)
     };
 
     /* parse command line options */
-    while ((i = getopt_long (argc, argv, "h:s:d:u", long_options, &option_index)) != -1) {
+    while ((i = getopt_long (argc, argv, "h:s:d:", long_options, &option_index)) != -1) {
         switch (i) {
             case 'h':
                 usage();
                 return -1;
-                break;
-
-            case 'u':
-                com_type = LGW_COM_USB;
                 break;
 
             case 'd':
@@ -169,16 +143,6 @@ int main(int argc, char **argv)
     sigaction( SIGINT, &sigact, NULL );
     sigaction( SIGTERM, &sigact, NULL );
 
-#if FULL_INIT
-    if (com_type == LGW_COM_SPI) {
-        /* Board reset */
-        if (system("./reset_lgw.sh start") != 0) {
-            printf("ERROR: failed to reset SX1302, check your reset_lgw.sh script\n");
-            exit(EXIT_FAILURE);
-        }
-    }
-#endif
-
     /* Initialize memory for capture */
     for (i = 0; i < CAPTURE_RAM_SIZE; i++) {
         capture_ram_buffer[i] = i%256;
@@ -190,86 +154,6 @@ int main(int argc, char **argv)
         return LGW_HAL_ERROR;
     }
 
-    /* Manual init */
-#if FULL_INIT
-    rf_radio_type[0] = LGW_RADIO_TYPE_SX1250;
-    rf_radio_type[1] = LGW_RADIO_TYPE_SX1257;
-    rf_enable[0] = false;
-    rf_enable[1] = true;
-    rf_clkout = 1;
-    rf_rx_freq[1] = 863700000;
-
-    /* setup radios */
-    for (i=0; i < 2; i++)
-    {
-        if (rf_enable[i] == true) {
-            sx1302_radio_reset(i, rf_radio_type[i]);
-            switch (radio_type) {
-                case LGW_RADIO_TYPE_SX1250:
-                    sx1250_setup(i, rf_rx_freq[i], false);
-                    break;
-                case LGW_RADIO_TYPE_SX1255:
-                case LGW_RADIO_TYPE_SX1257:
-                    sx125x_setup(i, rf_clkout, true, rf_radio_type[i], rf_rx_freq[i]);
-                    break;
-                default:
-                    DEBUG_MSG("ERROR: RADIO TYPE NOT SUPPORTED\n");
-                    return LGW_HAL_ERROR;
-            }
-            sx1302_radio_set_mode(i, radio_type);
-        }
-    }
-
-    /* Select the radio which provides the clock to the sx1302 */
-    sx1302_radio_clock_select(rf_clkout);
-
-    /* Check that the SX1302 timestamp counter is running */
-    lgw_get_instcnt(&val1);
-    lgw_get_instcnt(&val2);
-    if (val1 == val2) {
-        printf("ERROR: SX1302 timestamp counter is not running (val:%u)\n", (uint32_t)val1);
-        return -1;
-    }
-
-    /* Configure Radio FE */
-    sx1302_radio_fe_configure();
-
-    /* give radio control to AGC MCU */
-    lgw_reg_w(SX1302_REG_COMMON_CTRL0_HOST_RADIO_CTRL, 0x00);
-
-    /* Load firmware */
-    switch (rf_radio_type[rf_clkout]) {
-        case LGW_RADIO_TYPE_SX1250:
-            printf("Loading AGC fw for sx1250\n");
-            if (sx1302_agc_load_firmware(agc_firmware_sx1250) != LGW_HAL_SUCCESS) {
-                return LGW_HAL_ERROR;
-            }
-            if (sx1302_agc_start(FW_VERSION_AGC, SX1302_RADIO_TYPE_SX1250, SX1302_AGC_RADIO_GAIN_AUTO, SX1302_AGC_RADIO_GAIN_AUTO, 0) != LGW_HAL_SUCCESS) {
-                return LGW_HAL_ERROR;
-            }
-            break;
-        case LGW_RADIO_TYPE_SX1257:
-            printf("Loading AGC fw for sx125x\n");
-            if (sx1302_agc_load_firmware(agc_firmware_sx125x) != LGW_HAL_SUCCESS) {
-                return LGW_HAL_ERROR;
-            }
-            if (sx1302_agc_start(FW_VERSION_AGC, SX1302_RADIO_TYPE_SX125X, SX1302_AGC_RADIO_GAIN_AUTO, SX1302_AGC_RADIO_GAIN_AUTO, 0) != LGW_HAL_SUCCESS) {
-            // if (sx1302_agc_start(FW_VERSION_AGC, SX1302_RADIO_TYPE_SX125X, 1, 7, 0) != LGW_HAL_SUCCESS) {
-                return LGW_HAL_ERROR;
-            }
-            break;
-        default:
-            break;
-    }
-    printf("Loading ARB fw\n");
-    if (sx1302_arb_load_firmware(arb_firmware) != LGW_HAL_SUCCESS) {
-        return LGW_HAL_ERROR;
-    }
-    if (sx1302_arb_start(FW_VERSION_ARB) != LGW_HAL_SUCCESS) {
-        return LGW_HAL_ERROR;
-    }
-#endif
-
     // lgw_reg_w(SX1302_REG_CAPTURE_RAM_CLOCK_GATE_OVERRIDE_CLK_OVERRIDE, 3);
 
     /* Configure the Capture Ram block */
@@ -280,12 +164,9 @@ int main(int argc, char **argv)
     lgw_reg_w(SX1302_REG_CAPTURE_RAM_CAPTURE_SOURCE_A_SOURCEMUX, capture_source);
 
     printf("Sampling frequency: %d\n", sampling_frequency[capture_source]);
-    if (sampling_frequency[capture_source] != 0)
-    {
+    if (sampling_frequency[capture_source] != 0) {
         period_value = (32e6/sampling_frequency[capture_source]) - 1;
-    }
-    else
-    {
+    } else {
         fprintf(stderr ,"ERROR: Sampling frequency is null\n");
         return -1;
     }
@@ -305,7 +186,7 @@ int main(int argc, char **argv)
     // lgw_reg_w(SX1302_REG_CAPTURE_RAM_CAPTURE_CFG_CAPTUREFORCETRIGGER, 1);
 
     /* Poll Status.CapComplete */
-    do{
+    do {
         lgw_reg_r(SX1302_REG_CAPTURE_RAM_STATUS_CAPCOMPLETE, &val);
 
         wait_ms(10);
@@ -314,7 +195,6 @@ int main(int argc, char **argv)
         }
     } while (val != 1);
     lgw_reg_w(SX1302_REG_CAPTURE_RAM_CAPTURE_CFG_CAPTURESTART, 0);
-
 
     // lgw_reg_r(SX1302_REG_CAPTURE_RAM_LAST_RAM_ADDR_0_LASTRAMADDR, &val);
     // fprintf(stdout, "SX1302_REG_CAPTURE_RAM_LAST_RAM_ADDR_0_LASTRAMADDR value: %02x\n", val);
@@ -326,56 +206,34 @@ int main(int argc, char **argv)
     lgw_reg_w(SX1302_REG_COMMON_PAGE_PAGE, 0);
 
     printf("Data:\n");
-    for (i = 0; i < CAPTURE_RAM_SIZE; i += 4)
-    {
-        if (((capture_source >= 2) && (capture_source <= 3)) || (capture_source == 9))
-        {
+    for (i = 0; i < CAPTURE_RAM_SIZE; i += 4) {
+        if (((capture_source >= 2) && (capture_source <= 3)) || (capture_source == 9)) {
             real = (int16_t)((((uint16_t)(capture_ram_buffer[i+3]) << 8) & 0xFF00) + ((uint16_t)capture_ram_buffer[i+2] & 0x00FF));
             imag = (int16_t)((((uint16_t)(capture_ram_buffer[i+1]) << 8) & 0xFF00) + ((uint16_t)capture_ram_buffer[i+0] & 0x00FF));
-            real >>= 4;   // 12 bits I
-            imag >>= 4;   // 12 bits Q
-        }
-        else if ((capture_source >= 4) && (capture_source <= 6))
-        {
-            real = (int16_t)((((uint16_t)(capture_ram_buffer[i+3]) << 8) & 0xFF00) + ((uint16_t)capture_ram_buffer[i+2] & 0x00FF));    // 16 bits I
-            imag = (int16_t)((((uint16_t)(capture_ram_buffer[i+1]) << 8) & 0xFF00) + ((uint16_t)capture_ram_buffer[i+0] & 0x00FF));    // 16 bits Q
-        }
-        else if ((capture_source >= 10) && (capture_source <= 17))
-        {
-            real = (int8_t)(capture_ram_buffer[i+3]);    // 8 bits I
-            imag = (int8_t)(capture_ram_buffer[i+1]);    // 8 bits Q
-        }
-        else
-        {
+            real >>= 4; // 12 bits I
+            imag >>= 4; // 12 bits Q
+        } else if ((capture_source >= 4) && (capture_source <= 6)) {
+            real = (int16_t)((((uint16_t)(capture_ram_buffer[i+3]) << 8) & 0xFF00) + ((uint16_t)capture_ram_buffer[i+2] & 0x00FF)); // 16 bits I
+            imag = (int16_t)((((uint16_t)(capture_ram_buffer[i+1]) << 8) & 0xFF00) + ((uint16_t)capture_ram_buffer[i+0] & 0x00FF)); // 16 bits Q
+        } else if ((capture_source >= 10) && (capture_source <= 17)) {
+            real = (int8_t)(capture_ram_buffer[i+3]); // 8 bits I
+            imag = (int8_t)(capture_ram_buffer[i+1]); // 8 bits Q
+        } else {
             real = 0;
             imag = 0;
         }
 
-        if (((capture_source >= 2) && (capture_source <= 6)) || ((capture_source >= 9) && (capture_source <= 17)))
-        {
+        if (((capture_source >= 2) && (capture_source <= 6)) || ((capture_source >= 9) && (capture_source <= 17))) {
             fprintf(stdout, "%d", real);
-            if (imag >= 0)
-            {
+            if (imag >= 0) {
                 fprintf(stdout, "+");
             }
             fprintf(stdout, "%di\n", imag);
-        }
-        else
-        {
+        } else {
             printf("%02X ", capture_ram_buffer[i]);
         }
     }
     printf("End of Data\n");
-
-#if FULL_INIT
-    if (com_type == LGW_COM_SPI) {
-        /* Board reset */
-        if (system("./reset_lgw.sh stop") != 0) {
-            printf("ERROR: failed to reset SX1302, check your reset_lgw.sh script\n");
-            exit(EXIT_FAILURE);
-        }
-    }
-#endif
 
     return 0;
 }
